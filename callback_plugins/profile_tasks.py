e6900fe082b9fbc5f9fac75ffd5fe3a6b2944545
Zsolt Keserű <zsolooo@gmail.com>
2015-07-16 00:04:19 +0200

    rework role and total time calculation
    The name of the role now determined from the self.task.role_name
    variable instead of parsing the displayname of the task.
    This change needed fundamental changes in the calculation code.

--- callback_plugins/profile_tasks.py
+++ callback_plugins/profile_tasks.py
@@ -14,6 +14,9 @@ class CallbackModule(object):
         self.stats = {}
         self.current = None
         self.enabled = None
+        self.role_stats = {}
+        self.current_role = None
+        self.total_time = 0
 
     def playbook_on_task_start(self, name, is_conditional):
         """
@@ -32,10 +35,17 @@ class CallbackModule(object):
         if self.current is not None:
             # Record the running time of the last executed task
             self.stats[self.current] = time.time() - self.stats[self.current]
+            self.role_stats[self.current_role] += self.stats[self.current]
+            self.total_time += self.stats[self.current]
 
         # Record the start time of the current task
         self.current = name
+        self.current_role = self.task.role_name \
+            if self.task.role_name \
+            else '(no-role)'
         self.stats[self.current] = time.time()
+        if self.current_role not in self.role_stats:
+            self.role_stats[self.current_role] = 0
 
     def playbook_on_stats(self, stats):
         """
@@ -49,6 +59,8 @@ class CallbackModule(object):
         # Record the timing of the very last task
         if self.current is not None:
             self.stats[self.current] = time.time() - self.stats[self.current]
+            self.role_stats[self.current_role] += self.stats[self.current]
+            self.total_time += self.stats[self.current]
 
         # the raw results
         results = self.stats.items()
@@ -63,27 +75,14 @@ class CallbackModule(object):
         # Just keep the top 10
         sortedResults = sortedResults[:10]
 
-        # the total time for all of the roles/tasks
-        totalTime = 0
-        # the timings for the role
-        roleTimings = {}
-
         # Print the timings
         for name, elapsed in results:
-            # build up the role timings
-            strippedName = name[:name.find("|")]
-            if strippedName in roleTimings:
-                roleTimings[strippedName] = roleTimings[strippedName] + elapsed
-            else:
-                roleTimings[strippedName] = elapsed
-
             print(
                 "{0:-<70}{1:->9}".format(
                     '{0} '.format(name),
                     ' {0:.02f}s'.format(elapsed),
                 )
             )
-            totalTime = totalTime + elapsed
 
         # display the top 10 slowest tasks
         display(callbacks.banner("TOP 10 SLOWEST TASKS"))
@@ -99,7 +98,10 @@ class CallbackModule(object):
         # Display the times per role
         display(callbacks.banner("ROLE TIMES"))
         sortedRoleTimings = sorted(
-            roleTimings.iteritems(), key=lambda (k, v): (v, k), reverse=True)
+            self.role_stats.items(),
+            key=lambda value: value[1],
+            reverse=True,
+        )
         for role, timing in sortedRoleTimings:
             print(
                 "{0:-<70}{1:->9}".format(
@@ -113,6 +115,6 @@ class CallbackModule(object):
         print(
             "{0:-<70}{1:>9}".format(
                 '{0}'.format(""),
-                ' {0:.02f}s'.format(totalTime),
+                ' {0:.02f}s'.format(self.total_time),
             )
         )
